bfcde6114d9783458188a02778fe0da0885d6772
samuel <damosisamuel@gmail.com>
2021-02-22 17:46:29 +0100

    added index and css

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,6 @@
 let mix = require('laravel-mix');
 
-mix.js('src/app.js', 'dist').sass('src/app.scss', 'dist');
+mix.js('src/script.js', 'js').sass('src/style.scss', 'css')
+.options({
+  processCssUrls: false
+});
